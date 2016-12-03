96c5966cfa410b1ce2495127157d4e439a114bd6
in90210foq <in90210foq@gmail.com>
2016-12-03 08:44:29 +0000

    increase --max-results default from 25 to 50
    50 seems to be the api limit in the past?

--- y.py
+++ y.py
@@ -50,7 +50,7 @@ def youtube_search(options):
 
 if __name__ == "__main__":
   argparser.add_argument("--q", help="Search term", default="Google")
-  argparser.add_argument("--max-results", help="Max results", default=25)
+  argparser.add_argument("--max-results", help="Max results", default=50)
   args = argparser.parse_args()
 
   try:
